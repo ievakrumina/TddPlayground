ce5eecbd61d1b1ded82914182ec3cae00a8798ad
ievakrumina <ievakrumina93@gmail.com>
2024-09-01 14:30:27 +0200

    Cleanup fibonacci

--- src/test/kotlin/fibonacci/FibonacciTest.kt
+++ src/test/kotlin/fibonacci/FibonacciTest.kt
@@ -2,21 +2,27 @@ package fibonacci
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import org.example.fibonacci.Fibonacci
 
 class FibonacciTest {
 
     @Test
     fun fibonacci() {
-        val fib = Fibonacci()
         val testCases = listOf(
             Pair(0,0),
             Pair(1,1),
             Pair(2,1),
-            Pair(3, 2),
+            Pair(3,2),
         )
         testCases.forEach {
-            assertEquals(it.second, fib.f(it.first))
+            assertEquals(it.second, fib(it.first))
+        }
+    }
+
+    private fun fib(i: Int): Int {
+        return when (i) {
+            0 -> 0
+            1 -> 1
+            else -> fib(i - 1) + fib(i -2)
         }
     }
 }
