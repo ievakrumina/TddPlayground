49e56e31ba0ac7f247a3a38bb1226fb5416e59cc
ievakrumina <ievakrumina93@gmail.com>
2024-09-01 16:37:17 +0200

    Return "Buzz" when number is 5

--- src/test/kotlin/fizzbuzz/FizzBuzzTest.kt
+++ src/test/kotlin/fizzbuzz/FizzBuzzTest.kt
@@ -8,7 +8,7 @@ class FizzBuzzTest {
     /**
      * Requirements
      * [v] 1. Write a “fizzBuzz” method that accepts a number as input and returns it as a String.
-     * [] 2. For multiples of three return “Fizz” instead of the number
+     * [v] 2. For multiples of three return “Fizz” instead of the number
      * [] 3. For the multiples of five return “Buzz”
      * [] 4. For numbers that are multiples of both three and five return “FizzBuzz”.
      */
@@ -27,7 +27,16 @@ class FizzBuzzTest {
         assertEquals("Fizz", fizzBuzz(3*3))
     }
 
+    @Test
+    fun shouldReturnBuzz_whenInputIsMultipleOfFive() {
+        assertEquals("Buzz", fizzBuzz(5))
+    }
+
     private fun fizzBuzz(number: Int): String {
-        return if (number % 3 == 0 && number != 0) "Fizz" else number.toString()
+        return when {
+            number == 5 -> "Buzz"
+            number % 3 == 0 && number != 0 -> "Fizz"
+            else -> number.toString()
+        }
     }
 }
