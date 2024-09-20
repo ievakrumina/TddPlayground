3c49a67883483d7758911ab1e681fb7e0c421657
ievakrumina <ievakrumina93@gmail.com>
2024-09-20 14:56:13 +0200

    Hardcode returning $7.25

--- src/test/kotlin/barcodescanner/BarcodeScannerTest.kt
+++ src/test/kotlin/barcodescanner/BarcodeScannerTest.kt
@@ -1,5 +1,10 @@
 package barcodescanner
 
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
 class BarcodeScannerTest {
     /**
      * Create a simple app for scanning bar codes to sell products.
@@ -16,4 +21,21 @@ class BarcodeScannerTest {
      * []5. Introduce a concept of total command where it is possible to scan multiple items.
      * The command would display the sum of the scanned product prices
      */
+
+    @ParameterizedTest(name = "Should display price {0}, when barcode is {1}")
+    @MethodSource("source")
+    fun testScanBarcode(expectedPrice: String, barcode: String) {
+        assertEquals(expectedPrice, scanBarcode(barcode = barcode))
+    }
+
+    companion object {
+        @JvmStatic
+        fun source() = listOf(
+            Arguments.of("$7.25", "12345")
+        )
+    }
+
+    private fun scanBarcode(barcode: String): String {
+        return "$7.25"
+    }
 }
